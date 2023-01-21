f4a8287f22fc401ea88e37b0f02e398ac83b5fdd
Victor-Venegas-Calbillan <victort8012@gmail.com>
2023-01-20 21:02:45 -0300

    Agregando y configurando libreria next ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import { NextPage } from "next"
+import { Button } from '@nextui-org/react';
 
 const HomePage: NextPage = () => {
   return (
     <>
-      <h1>hola mundo</h1>
+      <Button color={"primary"}>Hola mundo</Button>
     </>
   )
 }
