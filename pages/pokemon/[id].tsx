2929423e759b7c579c32331ad0154f5f36c45c6e
Victor-Venegas-Calbillan <victort8012@gmail.com>
2023-01-24 02:06:05 -0300

    usando nuevo layout de PokemonPage

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -2,7 +2,7 @@ import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import { MainLayout } from "@/components/layouts"
 import { pokeApi } from '@/api';
 import { Pokemon } from '@/interfaces';
-import { Button, Card, Container, Grid, Image, Text } from '@nextui-org/react';
+import { PokemonPageLayout } from '@/components/layouts/PokemonPageLayout';
 
 interface Props {
   pokemon: Pokemon
@@ -11,61 +11,10 @@ interface Props {
 const PokemonPage: NextPage<Props> = ({ pokemon }) => {
 
   return (
-    <MainLayout title={pokemon.name}>
-      <Grid.Container css={{ marginTop: '5px' }} gap={ 2 }>
-        <Grid xs={ 12 } sm={ 4 }>
-          <Card isHoverable css={{ p: '20px'}}>
-            <Card.Image 
-              src={ pokemon.sprites.other?.dream_world.front_default || pokemon.sprites.front_default }
-              alt={ pokemon.name } 
-              width='100%' 
-              height={200}
-            />
-          </Card>
-        </Grid>
-        <Grid xs={ 12 } sm={ 8 }>
-          <Card isHoverable css={{ p: '20px'}}>
-            <Card.Header css={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap'}} >
-              <Text h1 transform='capitalize'>{ pokemon.name }</Text>
-              <Button
-               color='gradient'
-               ghost
-              >
-                Guardar en Favoritos
-              </Button>
-            </Card.Header>
-            <Card.Body>
-              <Text size={ 30 } >Sprites</Text>
-              <Container direction='row' display='flex'>
-                <Image
-                  src={ pokemon.sprites.front_default }
-                  alt={ pokemon.name }
-                  width={ 100 }
-                  height={ 100 }
-                />
-                <Image
-                  src={ pokemon.sprites.back_default }
-                  alt={ pokemon.name }
-                  width={ 100 }
-                  height={ 100 }
-                />
-                <Image
-                  src={ pokemon.sprites.front_shiny }
-                  alt={ pokemon.name }
-                  width={ 100 }
-                  height={ 100 }
-                />
-                <Image
-                  src={ pokemon.sprites.back_shiny }
-                  alt={ pokemon.name }
-                  width={ 100 }
-                  height={ 100 }
-                />
-              </Container>
-            </Card.Body>
-          </Card>
-        </Grid>
-      </Grid.Container>
+    <MainLayout title={ pokemon.name }>
+      <PokemonPageLayout pokemon={ pokemon } >
+        <h1>hola mundo</h1>
+      </PokemonPageLayout>
     </MainLayout>
   )
 }
