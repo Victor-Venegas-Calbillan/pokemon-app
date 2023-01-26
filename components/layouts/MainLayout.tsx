cb9ce38efafbf34cd5616bfae0199512d88f9190
Victor-Venegas-Calbillan <victort8012@gmail.com>
2023-01-25 22:46:13 -0300

    Agregando metatag conel tipo og para cuando el link se comparta

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -7,7 +7,11 @@ interface Props extends PropsWithChildren {
   title?: string
 }
 
+const origin = (typeof window === 'undefined') ? 'http://localhost:3000' : window.location.origin
+
 export const MainLayout: FC <Props> = ({ children, title }) => {
+
+  
   return (
     <>
       <Head>
@@ -15,6 +19,9 @@ export const MainLayout: FC <Props> = ({ children, title }) => {
         <meta name="author" content="Victor Venegas"  />
         <meta name="description" content={`Informacion sobre el pokemon ${ title }`}  />
         <meta name="keywords" content={`${title} ,pokemon, pokedex`}  />
+        <meta property="og:title" content={`Informacion sobre ${ title }`} />
+        <meta property="og:description" content={`Esta es la página sobre ${ title } `} />
+        <meta property="og:image" content={`${origin}/img/banner.png`} />
       </Head>
       <NavBar />
       <main>
